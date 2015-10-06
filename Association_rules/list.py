2459ae8d1ce5fb3e65438ca0c29d87e9b2bf01e6
Andrey Rybak <rybak@devexperts.com>
2015-10-06 18:19:27 +0300

    Association_rules: main - reading and aggregating of data list - update .gitignore : added idea IDE folder

--- Association_rules/list.py
+++ Association_rules/list.py
@@ -2,21 +2,20 @@ from ml import *
 import textwrap
 
 data = load_super_market_dataset()
-
-
-# print(data['description'])
+print(data.keys())
 print("Relation : " + str(data['relation']))
+print("Description : " + str(data['description']))
 
 attrs = data['attributes']
+for (x, y) in attrs:
+    print(x)
 print("Departments:")
 for (x, y) in attrs:
     if x == 'product_department':
-        pass
+        print(textwrap.indent(textwrap.fill(", ".join(map(str, y)), 80), "\t"))
 print("Categories:")
 for (x, y) in attrs:
     if x == 'product_category':
         print(textwrap.indent(textwrap.fill(", ".join(map(str, y)), 80), "\t"))
 
-for (x, y) in data['attributes']:
-    print(x)
 
--- Association_rules/main.py
+++ Association_rules/main.py
@@ -1,9 +1,20 @@
-import arff
-# pip install liac-arff for the arff module
+import textwrap
+from ml import *
 
-def load_super_market_dataset():
-    return arff.load(open('supermarket.arff', 'r'))
+big_data = load_super_market_dataset()
 
+attr = big_data['attributes']
+data = big_data['data']
 
+baskets = {}  # empty dictionary
+for (name, category, department, basket_id) in data:
+    baskets.setdefault(basket_id, [])
+    baskets[basket_id].append((name, category, department))
 
+print("Example of a basket:")
+example = "somebody is preparing dinner:\n" + str(", ".join(map(lambda x: x[0], baskets.get(data[2][3]))))
+print(textwrap.fill(example, 80))
 
+print("number of baskets = {}".format(len(baskets)))
+# for i, basket in enumerate(baskets.items()):
+#     print(i, basket[1])
