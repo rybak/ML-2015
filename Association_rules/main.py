4bf2f0eb1464380a9ffbded6689b69709e924a9f
Andrey Rybak <rybak@devexperts.com>
2015-10-06 16:36:14 +0300

    updating Association_rules load_dataset

--- Association_rules/main.py
+++ Association_rules/main.py
@@ -1,7 +1,9 @@
-
-
 import arff
+# pip install liac-arff for the arff module
 
 def load_super_market_dataset():
-    return arff.load('supermarket.arff')
+    return arff.load(open('supermarket.arff', 'r'))
+
+
+
 
